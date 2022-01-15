7fee7d42c63a15f179aa68aa32b69a67d1518d54
Marcello Sylvester Bauer <sylv@sylv.io>
2022-01-15 21:29:34 +0100

    mb/gpd/micropc: use IT8987E
    Change-Id: I90d1e4b3b9b1c2d6db527f823e9fb322f61ac446
    Signed-off-by: Marcello Sylvester Bauer <sylv@sylv.io>

--- src/mainboard/gpd/micropc/bootblock.c
+++ src/mainboard/gpd/micropc/bootblock.c
@@ -2,14 +2,11 @@
 
 #include <bootblock_common.h>
 #include <superio/ite/common/ite.h>
-#include <superio/ite/it8786e/it8786e.h>
+#include <superio/ite/it8987e/it8987e.h>
 
-#define GPIO_DEV	PNP_DEV(0x2e, IT8786E_GPIO)
-#define SERIAL1_DEV	PNP_DEV(0x2e, IT8786E_SP1)
-#define SERIAL3_DEV	PNP_DEV(0x2e, IT8786E_SP3)
-#define SERIAL4_DEV	PNP_DEV(0x2e, IT8786E_SP4)
-#define SERIAL5_DEV	PNP_DEV(0x2e, IT8786E_SP5)
-#define SERIAL6_DEV	PNP_DEV(0x2e, IT8786E_SP6)
+#define GPIO_DEV	PNP_DEV(0x2e, IT8987E_GPIO)
+#define SERIAL1_DEV	PNP_DEV(0x2e, IT8987E_SP1)
+#define SERIAL2_DEV	PNP_DEV(0x2e, IT8987E_SP2)
 
 void bootblock_mainboard_early_init(void)
 {
@@ -17,16 +14,4 @@ void bootblock_mainboard_early_init(void)
 	ite_enable_3vsbsw(GPIO_DEV);
 	ite_kill_watchdog(GPIO_DEV);
 	ite_enable_serial(SERIAL1_DEV, CONFIG_TTYS0_BASE);
-
-	/*
-	 * FIXME:
-	 * IT8786E has 6 COM ports, COM1/3/5 have default IO base 0x3f8 and
-	 * COM2/4/6 have 0x2f8. When enabling devices before setting resources
-	 * from devicetree, the output on debugging COM1 becomes very slow due
-	 * to the same IO bases for multiple COM ports. For now set different
-	 * hardcoded IO bases for COM3/4/5/6 ports, they will be set later to
-	 * desired values from devicetree. They can be also turned off.
-	 */
-	ite_enable_serial(SERIAL3_DEV, 0x3e8);
-	ite_enable_serial(SERIAL5_DEV, 0x2f0);
 }
