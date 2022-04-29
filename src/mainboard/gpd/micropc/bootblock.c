8328ea1bb4cca26807b0802ab0948908572ba7ab
Angel Pons <22937129+Th3Fanbus@users.noreply.github.com>
2022-04-29 17:54:39 +0000

    mb/gpd/micropc: Remove bogus ITE configuration
    The IT8987E does not have a GPIO LDN.

--- src/mainboard/gpd/micropc/bootblock.c
+++ src/mainboard/gpd/micropc/bootblock.c
@@ -4,14 +4,9 @@
 #include <superio/ite/common/ite.h>
 #include <superio/ite/it8987e/it8987e.h>
 
-#define GPIO_DEV	PNP_DEV(0x2e, IT8987E_GPIO)
-#define SERIAL1_DEV	PNP_DEV(0x2e, IT8987E_SP1)
 #define SERIAL2_DEV	PNP_DEV(0x2e, IT8987E_SP2)
 
 void bootblock_mainboard_early_init(void)
 {
-	ite_conf_clkin(GPIO_DEV, ITE_UART_CLK_PREDIVIDE_24);
-	ite_enable_3vsbsw(GPIO_DEV);
-	ite_kill_watchdog(GPIO_DEV);
 	ite_enable_serial(SERIAL2_DEV, CONFIG_TTYS0_BASE);
 }
